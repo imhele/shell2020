fbdbdbd32dd74169ac9c6d5e871d8a2c316dd6e2
imhele <work@imhele.com>
2020-03-05 13:38:22 +0800

    linklist: unshift / getter

--- src/utils/structs/linklist.h
+++ src/utils/structs/linklist.h
@@ -1,20 +1,44 @@
 #ifndef __HLIB_UTILS_STRUCTS_LINKLIST
 #define __HLIB_UTILS_STRUCTS_LINKLIST
+
+#include <stdarg.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include "../helpers/calloc.h"
 
 struct LinkList
 {
   void *value;
-  struct LinkList *prev;
+  struct LinkList *next;
 };
 
-struct LinkList *createLinkList(struct LinkList *current, void *value)
+struct LinkList *LinkListUnshift(struct LinkList *head, ...)
 {
-  struct LinkList *list = HLIB_CALLOC(struct LinkList);
-  list->value = value;
-  list->prev = current;
-  return list;
+  void *value;
+  va_list argv;
+  struct LinkList *curr;
+  struct LinkList *prev;
+  struct LinkList *new_head = HLIB_CALLOC(struct LinkList);
+
+  va_start(argv, head);
+  for (prev = new_head; value = va_arg(argv, void *); prev = curr)
+    (prev->next = curr = HLIB_CALLOC(struct LinkList))->value = value;
+  va_end(argv);
+
+  prev->next = head;
+  head = new_head->next;
+  free(new_head);
+
+  return head;
+}
+
+void *LinkListGetter(struct LinkList *head, int index)
+{
+  if (index < 0)
+    return NULL;
+  while (head && index--)
+    head = head->next;
+  return head ? head->value : NULL;
 }
 
 #endif /* __HLIB_UTILS_STRUCTS_LINKLIST */
