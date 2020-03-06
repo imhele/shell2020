cd57ab35e291904543370e6a072caa64585950e3
imhele <work@imhele.com>
2020-03-06 13:44:57 +0800

    chore: update code

--- src/commands/meta/options.h
+++ src/commands/meta/options.h
@@ -4,21 +4,21 @@
 #include <string.h>
 #include "../../utils/structs/map.h"
 
-struct CommandMetaOption
-{
-  char *name;
-  char *alias;
-  char *description;
-  struct Map *sub_options;
-};
-
 struct CommandMetaOptions
 {
   struct Map *name;
   struct Map *alias;
 };
 
-bool isCommandMetaOptionNameOrAliasEqual(void *a, void *b)
+struct CommandMetaOption
+{
+  char *name;
+  char *alias;
+  char *description;
+  struct CommandMetaOptions *sub_options;
+};
+
+bool __CommandMetaOptionIsNameOrAliasEqual(void *a, void *b)
 {
   return !strcmp((char *)a, (char *)b);
 }
@@ -29,10 +29,30 @@ struct CommandMetaOptions *CommandMetaOptionsRegister(
   if (options == NULL)
     options = HLIB_CALLOC(struct CommandMetaOptions);
 
-  MapSet(options->name, option->name, option, isCommandMetaOptionNameOrAliasEqual);
-  MapSet(options->alias, option->alias, option, isCommandMetaOptionNameOrAliasEqual);
+  MapSet(options->name, option->name, option, __CommandMetaOptionIsNameOrAliasEqual);
+  MapSet(options->alias, option->alias, option, __CommandMetaOptionIsNameOrAliasEqual);
 
   return options;
+}
+
+void CommandMetaOptionsFree(struct CommandMetaOptions **options);
+
+void __CommandMetaOptionFreeEach(void *value, int index, struct Map *curr)
+{
+  struct CommandMetaOption *option = (struct CommandMetaOption *)value;
+  if (option != NULL && option->sub_options != NULL)
+    CommandMetaOptionsFree(&(option->sub_options));
+}
+
+void CommandMetaOptionsFree(struct CommandMetaOptions **options)
+{
+  if (options == NULL || *options == NULL)
+    return;
+
+  LinkListFreeEach(&((*options)->name), __CommandMetaOptionFreeEach);
+  LinkListFreeEach(&((*options)->alias), __CommandMetaOptionFreeEach);
+
+  *options = NULL;
 }
 
 #endif /* __HLIB_COMMANDS_META_OPTIONS */
