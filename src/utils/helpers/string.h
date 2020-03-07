09610f2cdce27002fb0fa8faaabd44e9f3609d9b
imhele <work@imhele.com>
2020-03-07 20:30:22 +0800

    use strcpy instead of strcat

--- src/utils/helpers/string.h
+++ src/utils/helpers/string.h
@@ -4,16 +4,16 @@
 #include <string.h>
 #include "calloc.h"
 
-#define HLIB_STRCAT(dest, src)                                                        \
-  ({                                                                                  \
-    unsigned long __HLIB_STRCAT_LEN = ((dest) == NULL ? 0 : strlen((char *)(dest))) + \
-                                      ((src) == NULL ? 0 : strlen((char *)(src)));    \
-    char *__HLIB_STRCAT_DEST = HLIB_CALLOC_N(char, __HLIB_STRCAT_LEN);                \
-    if ((dest) != NULL)                                                               \
-      strcpy(__HLIB_STRCAT_DEST, (char *)(dest)), free((char *)(dest));               \
-    if ((src) != NULL)                                                                \
-      strcat(__HLIB_STRCAT_DEST, (char *)(src));                                      \
-    (dest) = (typeof(dest))__HLIB_STRCAT_DEST;                                        \
+#define HLIB_STRCAT(dest, src)                                                            \
+  ({                                                                                      \
+    unsigned long __STRCAT_SRC_LEN = (src) == NULL ? 0 : strlen((char *)(src));           \
+    unsigned long __STRCAT_DEST_LEN = (dest) == NULL ? 0 : strlen((char *)(dest));        \
+    char *__HLIB_STRCAT_DEST = HLIB_CALLOC_N(char, __STRCAT_SRC_LEN + __STRCAT_DEST_LEN); \
+    if (__STRCAT_DEST_LEN)                                                                \
+      strcpy(__HLIB_STRCAT_DEST, (char *)(dest)), free((char *)(dest));                   \
+    if (__STRCAT_SRC_LEN)                                                                 \
+      strcpy(__HLIB_STRCAT_DEST + __STRCAT_DEST_LEN, (char *)(src));                      \
+    (dest) = (typeof(dest))__HLIB_STRCAT_DEST;                                            \
   })
 
 char *HLIB_STRREPEAT(const char *str, int n)
@@ -21,9 +21,10 @@ char *HLIB_STRREPEAT(const char *str, int n)
   char *result;
   if (n <= 0)
     return result;
-  result = HLIB_CALLOC_N(char, strlen((char *)str) * (n));
-  while (n--)
-    strcat(result, str);
+  unsigned long len = strlen((char *)str);
+  result = HLIB_CALLOC_N(char, len *n);
+  for (int i = 0; i < n; i++)
+    strcpy(result + len * i, str);
   return result;
 }
 
