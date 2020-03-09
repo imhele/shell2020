2bd0e0d555c7a6e06d5847518dd99af9f1e6a3bc
imhele <work@imhele.com>
2020-03-09 16:15:00 +0800

    getch

--- src/commands.h
+++ src/commands.h
@@ -2,5 +2,6 @@
 #define __HLIB_COMMANDS
 
 #include "commands/meta.h"
+#include "commands/parser.h"
 
 #endif /* __HLIB_COMMANDS */
---
+++ src/commands/parser.h
@@ -0,0 +1,6 @@
+#ifndef __HLIB_COMMANDS_PARSER
+#define __HLIB_COMMANDS_PARSER
+
+#include "parser/getch.h"
+
+#endif /* __HLIB_COMMANDS_PARSER */
