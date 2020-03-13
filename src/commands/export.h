92a4252643983a5974d4b7fd697220f2a58e8bb6
imhele <work@imhele.com>
2020-03-13 15:19:29 +0800

    fix env vars are freed after cmd exec

--- src/commands/export.h
+++ src/commands/export.h
@@ -2,12 +2,13 @@
 #define __HLIB_COMMANDS_EXPORT
 
 #include <stdlib.h>
+#include "../utils/helpers/string.h"
 #include "_meta.h"
 
 int CommandExport(char **argv)
 {
   for (; *argv != NULL; argv++)
-    putenv(*argv);
+    putenv(HLIB_STRREPEAT(*argv, 1));
   return 0;
 }
 
